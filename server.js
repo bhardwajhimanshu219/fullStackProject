a159169318955ede7aab82470106a943c5a4dc79
hbharadwaj <hbharadwaj@ocrolus.com>
2022-01-23 21:57:48 +0530

    created separate folders for client server side

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+
+//coneectDB() function is basically used to connect mongodb atlas with our server
+
+connectDB();
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('API running');
 });
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/profile', require('./routes/profile'));
+app.use('/api/posts', require('./routes/posts'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port no ${PORT}`));
